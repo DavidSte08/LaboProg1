b5c170c20e7c3f73d066ed9e7432023fdbc9ba1a
David <uzf668@student.edu.ti.ch>
2025-09-26 16:22:57 +0200

    LinePrinter

--- HelloIntellij/src/Main.java
+++ HelloIntellij/src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello IDE");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000000; i++) {
             System.out.println(i);
         }
     }
