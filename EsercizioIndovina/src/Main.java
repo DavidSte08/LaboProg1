65f0aa67eec099514fbe1b3a4bde99372d5c0368
David <uzf668@student.edu.ti.ch>
2025-10-17 23:11:16 +0200

    Esercizi

--- EsercizioIndovina/src/Main.java
+++ EsercizioIndovina/src/Main.java
@@ -7,28 +7,29 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
 
-        int numero= 0;
+        int numero = 0;
         int numeroGenerato= rand.nextInt(101);
-
+        int conta = 0;
 
         System.out.print("Inserisci un numero: ");
         numero = input.nextInt();
             while(true) {
             if (numero == numeroGenerato) {
-                System.out.println("Numero indovinato");
+                conta = conta + 1;
+                System.out.println("Numero indovinato al " + conta + " tentativo");
                 break;
 
             } else if (numero < numeroGenerato) {
                 System.out.println("Numero troppo basso");
                 System.out.print("Inserisci un numero: ");
                 numero = input.nextInt();
-
+                conta = conta + 1;
 
             } else if (numero > numeroGenerato) {
                 System.out.println("Numero troppo alto");
                 System.out.print("Inserisci un numero: ");
                 numero = input.nextInt();
-
+                conta = conta + 1;
             }
         }
     }
