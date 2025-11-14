d3744c5e8411bf357077ce1673605aad29f3f360
David <uzf668@student.edu.ti.ch>
2025-11-14 16:24:11 +0100

    Esercizi

--- EsercizioMatrix/src/Main.java
+++ EsercizioMatrix/src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
     Matrice matrice = new Matrice();
-    Matrice matrice2 = new Matrice(7,6);
+    Matrice matrice2 = new Matrice(15,10);
 
-    System.out.println("righe " + matrice2.getRighe() + " colonne " + matrice.getColonne());
+    System.out.println("righe " + matrice2.getRighe() + " colonne " + matrice2.getColonne());
     }
 }
